6e61635330bfae18cd9cb036c62bfab29bd2aa61
Eliezer Yakovson <eliezeryakovson@gmail.com>
2025-12-11 12:57:49 +0200

    aa

--- main.py
+++ main.py
@@ -3,7 +3,21 @@ from core.utils import class_type, validate_mask
 
 
 def main():
-    print(class_type('11111111111111111111110000000000'))
+    ip_input = input("Enter IP address: ").strip()
+    mask_input = input("Enter Subnet Mask: ").strip()
+
+    if not validate_mask(mask_input):
+        print("Invalid subnet mask.")
+        return
+
+    network_info = network_investigation(ip_input, mask_input)
+
+    print(format_input_ip(ip_input))
+    print(format_subnet_mask(mask_input))
+    print(format_classful_status(network_info["type_class"]))
+    print(format_network_address(network_info["network_address"]))
+    print(format_broadcast_address(network_info["broadcast_address"]))
+   
 
 if __name__ == "__main__":
     main()
